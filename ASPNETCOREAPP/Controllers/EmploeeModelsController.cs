bcbb244cadf583bd6b52d6d4a4520d3b8b711a43
Giorgi Giorgobiani <giorgobiani086@gmail.com>
2020-08-07 14:07:32 +0400

    Add project files.

---
+++ ASPNETCOREAPP/Controllers/EmploeeModelsController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCOREAPP.Models;
+
+
+
+namespace ASPNETCOREAPP.Controllers
+{
+    public class EmploeeModelsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public EmploeeModelsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        
+
+        // GET: EmploeeModels
+        public async Task<IActionResult> Index(string SearchText)
+        {
+            var Emploees = from em in _context.Emploees
+                       select em;
+
+            if (!String.IsNullOrEmpty(SearchText))
+            {
+
+                Emploees = Emploees.Where(n => n.Name.Contains(SearchText) || n.Surname.Contains(SearchText) || (n.Name + " " + n.Surname).Contains(SearchText)|| (n.Surname + " " + n.Name).Contains(SearchText));
+                return View(await Emploees.ToListAsync());
+            }
+            else {
+                
+                return View(await _context.Emploees.ToListAsync());
+
+            }
+        }
+
+        // GET: EmploeeModels/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var emploeeModel = await _context.Emploees
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (emploeeModel == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(emploeeModel);
+        //}
+
+        // GET: EmploeeModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmploeeModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Empid,Name,Surname,Gender,DateofBirth,Email,Posti,Statusi,DateofFire")] EmploeeModel emploeeModel)
+        {
+            
+            if (ModelState.IsValid)
+            {
+
+                
+                bool checkmail = (from email in _context.Emploees
+                                  where email.Email == emploeeModel.Email
+                                  select email).Any();
+                
+
+                bool checkid = (from id in _context.Emploees
+                                where id.Empid == emploeeModel.Empid
+                                select id).Any();
+                if (checkmail)
+                {
+                    
+                    ViewBag.Message = "Aseti Meilit Registrirebuli tanamshromeli ukve arsebobs";
+                    return View(emploeeModel);
+                }
+                
+                if (checkid)
+                {
+                    ViewBag.Message = "Aseti tanamshromeli ukve arsebobs";
+                    return View(emploeeModel);
+                }
+
+
+
+                ViewBag.Message = "tanamshromeli Warmatebit Damaemata";
+                _context.Add(emploeeModel);
+                
+                await _context.SaveChangesAsync();
+
+                //return RedirectToAction(nameof(Index));
+
+                
+            }
+            
+            return View(emploeeModel);
+            
+        }
+
+        // GET: EmploeeModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var emploeeModel = await _context.Emploees.FindAsync(id);
+            if (emploeeModel == null)
+            {
+                return NotFound();
+            }
+            return View(emploeeModel);
+        }
+
+        // POST: EmploeeModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Empid,Name,Surname,Gender,DateofBirth,Email,Posti,Statusi,DateofFire")] EmploeeModel emploeeModel)
+        {
+            if (id != emploeeModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    bool checkmail = (from email in _context.Emploees
+                                      where email.Email == emploeeModel.Email
+                                      select email).Any();
+
+
+                    bool checkid = (from Id in _context.Emploees
+                                    where Id.Empid == emploeeModel.Empid
+                                    select Id).Any();
+                    if (checkmail)
+                    {
+
+                        ViewBag.Message = "Aseti Meilit Registrirebuli tanamshromeli ukve arsebobs";
+                        return View(emploeeModel);
+                    }
+
+                    if (checkid)
+                    {
+                        ViewBag.Message = "Aseti tanamshromeli ukve arsebobs";
+                        return View(emploeeModel);
+                    }
+
+
+                    _context.Update(emploeeModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmploeeModelExists(emploeeModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(emploeeModel);
+        }
+
+        // GET: EmploeeModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var emploeeModel = await _context.Emploees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (emploeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(emploeeModel);
+        }
+
+        // POST: EmploeeModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var emploeeModel = await _context.Emploees.FindAsync(id);
+            _context.Emploees.Remove(emploeeModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmploeeModelExists(int id)
+        {
+            return _context.Emploees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ASPNETCOREAPP/Startup.cs
@@ -0,0 +1,92 @@
+﻿using ASPNETCOREAPP.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private IConfiguration _Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+                
+            });
+            
+            
+
+
+            services.AddDbContextPool<DatabaseContext>(options =>
+            options.UseSqlServer(_Configuration.GetConnectionString("EmployeeDBConnection")));
+
+            //services.AddIdentity<ApplicationUserc, IdentityRole>(config =>
+            //{
+            //    config.SignIn.RequireConfirmedEmail = true;
+            //}
+            //).AddEntityFrameworkStores<DatabaseContext>().AddErrorDescriber<CustomIdentityErrorDescriber>();
+
+            services.AddIdentity<ApplicationUserc, IdentityRole>().AddEntityFrameworkStores<DatabaseContext>().AddErrorDescriber<CustomIdentityErrorDescriber>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.Configure<IdentityOptions>(options =>
+            //{
+            //    options.User.RequireUniqueEmail = true;
+
+            //});
+            
+            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
